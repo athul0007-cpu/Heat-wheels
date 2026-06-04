aceb72721a08c435575d081811cce0b08658453b
athul <aswasthat@gmail.com>
2026-06-04 21:29:13 +0530

    Configure Django project for Render.com deployment

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files so WhiteNoise can serve them
-DJANGO_SETTINGS_MODULE=hotwheels.settings.production python manage.py collectstatic --no-input
+DJANGO_SETTINGS_MODULE=hotwheels_project.settings.production python manage.py collectstatic --no-input
 
 # Run database migrations
-DJANGO_SETTINGS_MODULE=hotwheels.settings.production python manage.py migrate
+DJANGO_SETTINGS_MODULE=hotwheels_project.settings.production python manage.py migrate
 
 # Create superuser if needed
-DJANGO_SETTINGS_MODULE=hotwheels.settings.production python deploy_setup.py
+DJANGO_SETTINGS_MODULE=hotwheels_project.settings.production python deploy_setup.py
--- hotwheels_project/settings/production.py
+++ hotwheels_project/settings/production.py
@@ -1,12 +1,17 @@
+import os
 from .base import *
 
 DEBUG = False
 
 # Allowed hosts for production
 ALLOWED_HOSTS = ['heatwheels.onrender.com', 'localhost', '127.0.0.1']
+if 'RENDER_EXTERNAL_HOSTNAME' in os.environ:
+    ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
 
 # CSRF trusted origins for production
 CSRF_TRUSTED_ORIGINS = ['https://heatwheels.onrender.com']
+if 'RENDER_EXTERNAL_HOSTNAME' in os.environ:
+    CSRF_TRUSTED_ORIGINS.append('https://' + os.environ['RENDER_EXTERNAL_HOSTNAME'])
 
 # In production, set the database connection via environment variables.
 DATABASES['default'] = env.db('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
